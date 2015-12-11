7aa5954ad72620d4a7ac1618f280f90bed2fb9b1
Kato Yusuke <yu-tenvi1025@docomo.ne.jp>
2015-12-11 19:43:40 +0900

    first commit

---
+++ shippo.js
@@ -0,0 +1,12 @@
+ZGN(function()
+{
+	function(){};
+	function(){};
+	function(){};
+	function(){};
+	function(){};
+	function(){};
+	function(){};
+	function(){};
+
+});
---
+++ shippo_test.js
@@ -0,0 +1,27 @@
+ZGN(function()
+{
+	// 18番ピンで動作させます
+  var ledPin = '18';
+
+  // TerminalのGPIOインスタンスを取得します
+  var gpio = ZGN.term('1').gpio;
+
+  // 調光の初期値を0に設定します
+  var level = 0;
+
+  // 指定ピンをPWMに設定
+  gpio.pinMode(ledPin, ZGN.PWM);
+
+  // button1ボタンをクリック
+  $(document).on('click', '#t1', function() {
+    level = level >= 100 ? 100 : level + 1; // levelを1上げる
+    gpio.pwmWrite(ledPin, level / 100); // デューティー比の設定
+  });
+
+  // BUTTON2ボタンをクリック
+  $(document).on('click', '#T2', function() {
+    level = level <= 0 ? 0 : level - 1; // levelを1下げる
+    gpio.pwmWrite(ledPin, level / 100); // デューティー比の設定
+  });
+
+});
