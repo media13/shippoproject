f7c306dcebf9276e1f2a545f9ce1e5abe6d27528
Kato Yusuke <yu-tenvi1025@docomo.ne.jp>
2015-12-21 16:17:07 +0900

    no message

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 ZGN(function()
 {
-  // 18番ピンで動作させます
-  var ledPin = '18';
+  // 17番ピンで動作させます
+  var ledPin = '17';
 
   // TerminalのGPIOインスタンスを取得します
   var gpio = ZGN.term(1).gpio;
 
-  // 調光の初期値を0に設定します
-  var level = 0;
+  // 指定ピンを出力に設定
+  gpio.pinMode(ledPin, ZGN.OUTPUT);
 
-  // 指定ピンをPWMに設定
-  gpio.pinMode(18, ZGN.PWM);
-
-  // Brightボタンをクリック
-  $(document).on('click', '#bright', function() {
-    level = level >= 10 ? 10 : level + 1; // levelを1上げる
-    gpio.pwmWrite(ledPin, level / 10); // デューティー比の設定
-    alert(level);
+  // ONボタンをクリック
+  $(document).on('click', '#on', function() {
+    gpio.digitalWrite(ledPin, ZGN.HIGH); // 点灯
   });
 
-  // Darkボタンをクリック
-  $(document).on('click', '#dark', function() {
-    level = level <= 0 ? 0 : level - 1; // levelを1下げる
-    gpio.pwmWrite(ledPin, level / 10); // デューティー比の設定
-    alert(level);
+  // OFFボタンをクリック
+  $(document).on('click', '#off', function() {
+    gpio.digitalWrite(ledPin, ZGN.LOW); // 消灯
   });
 });
