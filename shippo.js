fcdaf27c385537ee54b1a66dde26ef26026df06e
Kato Yusuke <yu-tenvi1025@docomo.ne.jp>
2016-01-06 20:48:39 +0900

    no message

--- shippo.js
+++ shippo.js
@@ -1,29 +1,49 @@
 ﻿ZGN(function()
 {
 
-  // モータの初期値を90radに設定します
-  var rad = 90;
+/*--------------------------------------------------------------------
+  ターミナル
+--------------------------------------------------------------------*/
 
-  // 18番ピンで動作させます
-  var ledPin = '1';
+  var ledPin = '1';						// 18番ピンで動作させます
+  var term   = ZGN.term('1'); 		// id=1のTerminalインスタンスを取得
+  var gpio   = term.gpio;				// TerminalのGPIOインスタンスを取得
+  gpio.pinMode(ledPin, ZGN.PWM);		// 指定ピンを出力に設定
 
-  // id=1のTerminalインスタンスを取得します
-  var term = ZGN.term('1');
+/*--------------------------------------------------------------------
+  モータ
+--------------------------------------------------------------------*/
 
-  // TerminalのGPIOインスタンスを取得します
-  var gpio = term.gpio;
+  var pwm_sycle = 20;		// PWM周期
+  var pulse_min = 0.5;		// パルス制御の最小（rad = 0  ）
+  var pulse_max = 2.4;		// パルス制御の最大（rad = 180）
+  var pulse_unit = pulse_max - pulse_min / 180;
+									// 単位ラジアン当たりのパルス制御
 
-  // 指定ピンを出力に設定
-  gpio.pinMode(ledPin, ZGN.PWM);
+  var rad       = 90;		// モータの初期位置
 
-  // 
+/*--------------------------------------------------------------------
+  モータ制御
+--------------------------------------------------------------------*/
 
   // モータの角度をセットする。
   function ServoSet(radian){
-    
+    var rad = pulse_min + pulse_unit * radian;
+    var i = 0;
+    var servo = setInterval(function(){
+      var servo_on = setInterval(function(){
+        gpio.digitalWrite(ledPin, ZGN.HIGH);
+        clearInterval(servo_on);
+      }, calPalse(rad));
+      gpio.digitalWrite(ledPin, ZGN.LOW);
+    }, pwm_sycle);
   }
 
-  ServoSet(rad);
+/*--------------------------------------------------------------------
+  メイン
+--------------------------------------------------------------------*/
+
+  ServoSet(rad);		// モータを初期位置に設定
 
   // 1ボタンをクリック
   $(document).on('click', '#1', function() {
@@ -47,5 +67,10 @@
   $(document).on('click', '#-10', function() {
     rad = rad <= 0 ? 0 : rad - 10; // モータを-10radする
     ServoSet(rad);
+  });
+
+  // stopボタンをクリック
+  $(document).on('click', '#stop', function() {
+    gpio.digitalWrite(ledPin, ZGN.LOW);
   });
 });
