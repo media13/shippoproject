1c17cd9746678f661eb3232203fa52dfad50dc44
Kato Yusuke <yu-tenvi1025@docomo.ne.jp>
2016-01-10 11:23:05 +0900

    no message

---
+++ dc_motor.js
@@ -0,0 +1,64 @@
+﻿ZGN(function()
+{
+
+/*--------------------------------------------------------------------
+  ターミナル
+--------------------------------------------------------------------*/
+
+  var gpio        = ZGN.term('1').gpio;	// TerminalのGPIOインスタンスを取得
+
+  // モータ１
+  var motor_out11 = '4';						// 23番ピンで動作させます
+  var motor_out12 = '5';						// 24番ピンで動作させます
+  var motor_pwm1  = '1';						// 18番ピンで動作させます
+
+  gpio.pinMode(motor_out11, ZGN.OUTPUT);	// 指定ピンを出力に設定
+  gpio.pinMode(motor_out12, ZGN.OUTPUT);	// 指定ピンを出力に設定
+  gpio.pinMode(motor_pwm1, ZGN.PWM);		// 指定ピンをPWMに設定
+
+  // モータ２
+  var motor_out21 = '21';						// 5番ピンで動作させます
+  var motor_out22 = '22';						// 6番ピンで動作させます
+  var motor_pwm2  = '23';						// 13番ピンで動作させます
+
+  gpio.pinMode(motor_out21, ZGN.OUTPUT);	// 指定ピンを出力に設定
+  gpio.pinMode(motor_out22, ZGN.OUTPUT);	// 指定ピンを出力に設定
+  gpio.pinMode(motor_pwm2, ZGN.PWM);		// 指定ピンをPWMに設定
+
+/*--------------------------------------------------------------------
+  モータ制御
+--------------------------------------------------------------------*/
+
+  var pwm_power = 1; // pwmの強さ
+
+  function motor1(){
+    var motor = setInterval(function(){
+      gpio.digitalWrite(motor_out11, ZGN.HIGH);
+      gpio.pwmWrite(motor_out11, pwm_power);
+    }, 5000);
+
+    gpio.digitalWrite(motor_out11, ZGN.LOW);
+    gpio.pwmWrite(motor_out11, 0);
+  }
+
+//  function motor1(radian){}  // 角度追加
+//  function motor1(radian, time){} // 動作時間追加
+
+//  function joy         (){}
+//  function trust       (){}
+//  function fear        (){}
+//  function surprise    (){}
+//  function sadness     (){}
+//  function disgust     (){}
+//  function anger       (){}
+//  function anticipation(){}
+
+/*--------------------------------------------------------------------
+  メイン
+--------------------------------------------------------------------*/
+
+  // onボタンをクリック
+  $(document).on('click', '#on', function() {
+    motor1();
+  });
+});
---
+++ led.js
@@ -0,0 +1,20 @@
+﻿ZGN(function()
+{
+  // 17番ピンで動作させます
+  var ledPin = '0';
+
+  // TerminalのGPIOインスタンスを取得します
+  var gpio = ZGN.term('1').gpio;
+
+  // 調光の初期値を0に設定します
+  var level = 7.5;
+
+  // 指定ピンをPWMに設定
+  gpio.pinMode(ledPin, ZGN.PWM);
+
+  // Brightボタンをクリック
+  $(document).on('click', '#bright', function() {
+    level = level >= 10 ? 10 : level + 0.5; // levelを1上げる
+    gpio.pwmWrite(ledPin, 0.05); // デューティー比の設定
+  });
+});
